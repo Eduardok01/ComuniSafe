9a3a04bd136a8a49b2337896face43dc40e1de30
MarceloRebolledo <m.rebolledo17@ufromail.cl>
2025-05-25 19:28:30 -0400

    login view

--- comuni_safe_front/lib/main.dart
+++ comuni_safe_front/lib/main.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+import 'views/login/login_view.dart';
 
 void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ComuniSafe',
+      home: const LoginView(),
+    );
+  }
 }
