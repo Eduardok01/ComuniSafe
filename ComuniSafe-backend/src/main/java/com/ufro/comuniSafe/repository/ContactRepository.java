0f6613942199270330544f8dd6be15b2285f6deb
MarceloRebolledo <m.rebolledo17@ufromail.cl>
2025-05-31 19:59:26 -0400

    Delete ComuniSafe-backend directory

--- ComuniSafe-backend/src/main/java/com/ufro/comuniSafe/repository/ContactRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ufro.comuniSafe.repository;
-
-import com.ufro.comuniSafe.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
