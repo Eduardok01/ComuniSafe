269bea8e8ab2d9200753e324e3f5c1354b8f74e1
MarceloRebolledo <m.rebolledo17@ufromail.cl>
2025-06-05 12:19:16 -0400

    buenas practicas para login backend

--- ufro/src/main/java/com/ufro/service/UsuarioService.java
+++ ufro/src/main/java/com/ufro/service/UsuarioService.java
@@ -1,23 +1,32 @@
 package com.ufro.service;
 
+import com.ufro.dto.RegisterRequest;
 import com.ufro.model.Usuario;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
+
 @Service
 public class UsuarioService {
 
-
     private static final String COLLECTION_NAME = "usuarios";
+    private final FirebaseAuthService authService;
+
+    public UsuarioService(FirebaseAuthService authService) {
+        this.authService = authService;
+    }
 
     public void guardarUsuario(Usuario usuario) {
         Firestore db = FirestoreClient.getFirestore();
-        db.collection(COLLECTION_NAME).add(usuario);
+        db.collection(COLLECTION_NAME)
+                .document(usuario.getUid())
+                .set(usuario);
     }
 
     public List<Usuario> obtenerTodos() {
@@ -32,5 +41,30 @@ public class UsuarioService {
             e.printStackTrace();
         }
         return usuarios;
+    }
+
+    public Map<String, Object> loginConToken(String authorizationHeader) throws FirebaseAuthException {
+        String idToken = authorizationHeader.replace("Bearer ", "").trim();
+        FirebaseToken decodedToken = authService.verifyIdToken(idToken);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("uid", decodedToken.getUid());
+        response.put("email", decodedToken.getEmail());
+        return response;
+    }
+
+    public Usuario registrarConToken(String authorizationHeader, RegisterRequest request) throws FirebaseAuthException {
+        String idToken = authorizationHeader.replace("Bearer ", "").trim();
+        FirebaseToken decodedToken = authService.verifyIdToken(idToken);
+
+        Usuario usuario = new Usuario();
+        usuario.setUid(decodedToken.getUid());
+        usuario.setCorreo(decodedToken.getEmail());
+        usuario.setName(request.getName());
+        usuario.setPhone(request.getPhone());
+        usuario.setRol("user");
+
+        guardarUsuario(usuario);
+        return usuario;
     }
 }
