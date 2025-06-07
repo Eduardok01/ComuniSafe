f9615b392b3e90502bda61f33ffc22857129fdfc
EduardoKrause <e.krause01@ufromail.cl>
2025-06-07 18:00:21 -0400

    fix verificacion de usuario
    Se corrigio el acceso al endpoint para borrar usuario, ahora solo un administrador puede acceder a esta funcion.

--- ufro/src/main/java/com/ufro/controller/AdminController.java
+++ ufro/src/main/java/com/ufro/controller/AdminController.java
@@ -2,12 +2,14 @@ package com.ufro.controller;
 
 import com.ufro.service.UsuarioService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -29,13 +31,22 @@ public class AdminController {
         }
     }
     @DeleteMapping("/usuarios/{uid}")
-    public ResponseEntity<String> borrarUsuario(@PathVariable String uid) {
+    public ResponseEntity<String> eliminarUsuario(
+            //esta parte se encarga de proteger el endpoint, solo un admin puede borrar usuario
+            //en caso de no estar seguro, se puede quitar
+            @RequestHeader("Authorization") String authorizationHeader,
+            @PathVariable String uid) {
         try {
+            Map<String, Object> claims = usuarioService.loginConToken(authorizationHeader);
+            String role = (String) claims.get("role");
+            if (!"admin".equals(role)) {
+                return ResponseEntity.status(403).body("No autorizado");
+            }
+
             usuarioService.borrarUsuario(uid);
             return ResponseEntity.ok("Usuario eliminado correctamente");
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Error al eliminar usuario: " + e.getMessage());
         }
     }
-
 }
