12ea48ec771be5feb08bc0436875d610b9d60783
CarlosCienfuegos1 <c.cienfuegos01@ufromail.cl>
2025-06-14 05:27:45 -0400

    Reportes y Comentarios corregidos

--- ufro/src/main/java/com/ufro/model/Comentario.java
+++ ufro/src/main/java/com/ufro/model/Comentario.java
@@ -1,17 +1,15 @@
 package com.ufro.model;
 
-import com.google.type.DateTime;
+import com.google.cloud.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Comentario {
-    private String id;
     private String texto;
     private Timestamp fechaHora;
     private String usuarioId;
